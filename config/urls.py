b890cd322612c7e927e5bfa4b7ed2dee0ec3f265
gkswk117 <gkswk117@gmail.com>
2023-05-21 03:02:47 +0900

    [풀스택 에어비앤비 클론코딩] #9.0 Views (09:18) 완료.

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from rooms.views import say_hello
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rooms',say_hello )
 ]
