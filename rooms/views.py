87c9d8ca034ded231804c6bcd4586b5c990733b1
gkswk117 <gkswk117@gmail.com>
2023-05-21 04:06:56 +0900

    [풀스택 에어비앤비 클론코딩] #9.3 render (05:19) 완료.

--- rooms/views.py
+++ rooms/views.py
@@ -5,9 +5,13 @@ from .models import Room
 def see_all_rooms(request):
     print(f"{request}\n")
     print(f"{dir(request)}\n")
-    for each in Room.objects.all():
+    rooms = Room.objects.all()
+    print(rooms[1].name)
+    for each in rooms:
         print(each)
-    return HttpResponse("hello")
+    return render(request, "all_rooms.html", {'rooms':rooms, 'title':'hello'})
+# render 함수는 두번째 인자로 render할 html파일을 받고 그 파일을 자동으로 앱 폴더의 "templates" 폴더 내에서 찾는다.
+# 그래서 폴더명을 너 마음대로 정할 수 없다.
 
 def see_one_room(request, room_id):
     # url에서 변수를 넘겨받을 때, urls.py에 적힌 이름과 같은 이름을 써줘야 한다.
