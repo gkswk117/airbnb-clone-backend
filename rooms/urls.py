9e0233dab7030401ebf8b7232816d02ffef9efe7
gkswk117 <gkswk117@gmail.com>
2023-05-22 02:20:42 +0900

    [풀스택 에어비앤비 클론코딩] #9.5 DoesNotExist (08:54) 완료.

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import see_all_rooms, see_one_room
 urlpatterns = [
     path('', see_all_rooms),
-    path('<int:room_id>', see_one_room)
+    path('<int:room_pk>', see_one_room)
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -9,12 +9,23 @@ def see_all_rooms(request):
     print(rooms[1].name)
     for each in rooms:
         print(each)
-    return render(request, "all_rooms.html", {'rooms':rooms, 'title':'hello'})
+    return render(request, "all_rooms.html", {'rooms':rooms, 'title':'모든 방 리스트입니다.'})
 # render 함수는 두번째 인자로 render할 html파일을 받고 그 파일을 자동으로 앱 폴더의 "templates" 폴더 내에서 찾는다.
 # 그래서 폴더명을 너 마음대로 정할 수 없다.
 
-def see_one_room(request, room_id):
+def see_one_room(request, room_pk):
     # url에서 변수를 넘겨받을 때, urls.py에 적힌 이름과 같은 이름을 써줘야 한다.
-    print(room_id)
-    print(Room.objects.get(pk=room_id))
-    return HttpResponse("hi")
+    # javascript의 try, catch 구문이랑 비슷하다.
+    try:
+        room = Room.objects.get(pk=room_pk)
+        print(room_pk)
+        print(room)
+        print(dir(Room.DoesNotExist))
+        return render(request, "one_room.html", {'room':room, 'is_found':True})
+    except Room.DoesNotExist:
+        #Room.DoesNotExist는 에러의 한 종류
+        #Room.DoesNotExist라는 에러가 발생 했을때 이 블록을 실행한다.
+        #에러의 종류 중에는 ZeroDivisionError, IndexError 등 여러 가지가 있고, Room.DoesNotExist는 그 중 하나이다.
+        #참고: https://blog.naver.com/star7sss/222444006350
+        return render(request, "one_room.html", {'is_found':False})
+    
