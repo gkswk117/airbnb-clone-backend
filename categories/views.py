d28e1c1e5d6f264bfc2a419e52a20c2ad6e2ffcb
gkswk117 <gkswk117@gmail.com>
2023-05-23 13:44:10 +0900

    [풀스택 에어비앤비 클론코딩] #10.2 api_view (06:56) 완료

--- categories/views.py
+++ categories/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import  JsonResponse
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 # Create your views here.
-def categories(request):
+# Django rest Framework를 사용하기 전에 수동적으로 하는 방법을 먼저 배울 것.
+
+# noob
+def categories_noob(request):
     all_categories= Category.objects.all()
-    #return render(request, "hhh.html", {'categories':all_categories, 'title':'모든 카테고리 리스트입니다.'})
-    return JsonResponse({'ok':True})
+    # return render(request, "hhh.html", {'categories':all_categories, 'title':'모든 카테고리 리스트입니다.'})
+    return JsonResponse({'ok':True, "categories":serializers.serialize("json",all_categories)})
+    # serializers가 all_categories를 json 형식으로 바꿔준다.
+
+# pro
+@api_view()
+def categories(request):
+    return Response({'ok':True,})
