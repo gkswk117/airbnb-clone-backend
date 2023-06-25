c7e55582b92bea50fdedcbcac628b3927f18a686
Hanwoong <gkswk117@gmail.com>
2023-06-25 15:26:48 +0900

    [풀스택 에어비앤비 클론코딩] #12.1 Create User (11:52) 완료.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('', views.User.as_view()),
     path('mypage', views.MyPage.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ParseError
 from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError, PermissionDenied
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import User
@@ -17,6 +18,25 @@ class MyPage(APIView):
         serializer = PrivateUserSerializer(user, data=request.data, partial=True)
         if serializer.is_valid():
             user = serializer.save()
+            return Response(PrivateUserSerializer(user).data)
+        else:
+            return Response(serializer.errors)
+
+class User(APIView):
+    def post(self, request):
+        password = request.data.get('password')
+        if not password:
+            raise ParseError
+        serializer = PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            print("이건됨?")
+            user = serializer.save()
+            # password는 save()메소드 안에 넣어주는게 아니라 AbstractUser 클래스의 set_password() 메소드를 이용할 것.
+            user.set_password(password)
+            print("이건됨???")
+            user.save()
+            
+            # 이러면 django는 자동으로 password를 hash화 시켜준다.
             return Response(PrivateUserSerializer(user).data)
         else:
             return Response(serializer.errors)
