9e8f49b2f45586714c3f51887faaf1134bf305fb
Hanwoong <gkswk117@gmail.com>
2023-06-27 16:20:00 +0900

    [풀스택 에어비앤비 클론코딩] #16.1 Our First Test (06:22) 완료.

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class TestAmenities(APITestCase):
+    def test_two_plus_two(self):
+        self.assertEqual(2+2,5,"The calculation is wrong.")
